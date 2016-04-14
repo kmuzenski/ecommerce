9f1b6bf3f67744c9d412b522801de20de9556bef
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-04-13 19:11:42 -0500

    moving search

--- nav.php
+++ nav.php
@@ -86,16 +86,15 @@
 			<li><a href="update.php">Update</a></li>	
 			<li><a href="admin.php">Admin</a></li>
 		<?php } ?>
-
+		<li>
+			    	<form>
+<input type="text" size="30" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
+</form>
+</li>
 	   
 			</ul>
         	</div>
       	</div>
-      	<div>
-      	<form>
-<input type="text" size="30" onkeyup="showResult(this.value)">
-<div id="livesearch"></div>
-</form>
-
-</div>
+    
     	</nav>
