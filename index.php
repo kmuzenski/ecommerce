0397e32b6f56f716179d2bcf6cfd0301be07bf44
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-04-13 20:03:51 -0500

    moving search

--- index.php
+++ index.php
@@ -19,10 +19,7 @@
 </center>
 <br><br><br><br>
 
-			    	<form>
-<input type="text" size="30" onkeyup="showResult(this.value)">
-<div id="livesearch"></div>
-</form>
+		
 
 	
 <div class="container">
@@ -57,35 +54,7 @@ src="assets/img/trending.png" width="500"></a>
 <br><br><br><br><br>
 
 <?php require_once('footer.php'); ?>
-<script>
-function showResult(str) {
-	if (str.length==0) {
-	document.getElementById("livesearch").innerHTML="";
-	document.getElementById("livesearch").style.border="0px";
-	return;
-	}
 
-if (window.XMLHttpRequest) {
-//IE7 Ffox chrome safari opera
-
-	xmlhttp = new XMLHttpRequest();
-
-} else {
-//IE - 6
-
-	xmlhttp = new ActiveXObject ("Microsoft.XMLHTTP");
-}
-
-xmlhttp.onreadystatechange = function() {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-	document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
-	document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-	}
-}
-xmlhttp.open("GET","livesearch.php?q="+str,true);
-xmlhttp.send();
-
-}</script>
 
   </body>
 </html>
--- nav.php
+++ nav.php
@@ -91,5 +91,38 @@
 			</ul>
         	</div>
       	</div>
+      	<script>
+function showResult(str) {
+	if (str.length==0) {
+	document.getElementById("livesearch").innerHTML="";
+	document.getElementById("livesearch").style.border="0px";
+	return;
+	}
+
+if (window.XMLHttpRequest) {
+//IE7 Ffox chrome safari opera
+
+	xmlhttp = new XMLHttpRequest();
+
+} else {
+//IE - 6
+
+	xmlhttp = new ActiveXObject ("Microsoft.XMLHTTP");
+}
+
+xmlhttp.onreadystatechange = function() {
+	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+	document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
+	document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+	}
+}
+xmlhttp.open("GET","livesearch.php?q="+str,true);
+xmlhttp.send();
+
+}</script>
+	    	<form>
+<input type="text" size="30" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
+</form>
     
     	</nav>
