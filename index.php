d00c769616a2869f15f74e529e2d4e84842f89cd
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-04-13 19:17:30 -0500

    search

--- index.php
+++ index.php
@@ -57,35 +57,8 @@ src="assets/img/trending.png" width="500"></a>
 <br><br><br><br><br>
 
 <?php require_once('footer.php'); ?>
-<script>
-function showResult(str) {
-	if (str.length==0) {
-	document.getElementById("livesearch").innerHTML="";
-	document.getElementById("livesearch").style.border="0px";
-	return;
-	}
+<script src="search.js">
 
-if (window.XMLHttpRequest) {
-//IE7 Ffox chrome safari opera
-
-	xmlhttp = new XMLHttpRequest();
-
-} else {
-//IE - 6
-
-	xmlhttp = new ActiveXObject ("Microsoft.XMLHTTP");
-}
-
-xmlhttp.onreadystatechange = function() {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-	document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
-	document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-	}
-}
-xmlhttp.open("GET","livesearch.php?q="+str,true);
-xmlhttp.send();
-
-}
 </script>
 
   </body>
