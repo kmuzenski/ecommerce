9b6ba8a98ad555f8480faeb10c9de1bc90bf4b53
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-04-16 14:36:07 -0500

    working on search

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>Kiss N' Makeup &copy;<a href="sitemap.php">site map</a></footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
+<script src="searchJS.js"></script>
 
