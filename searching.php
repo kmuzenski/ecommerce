b0ca03dea37fa3e2b85362aa5f0ffe946d8bf9db
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-02 18:51:12 -0500

    style change

--- searching.php
+++ searching.php
@@ -1,20 +1,20 @@
 <?php
-include('database.php');
+require_once('database.php');
 if($_POST)
 {
+    $pdo = Database::connect();
+    
     $q = mysqli_real_escape_string($connection,$_POST['search']);
     $strSQL_Result = mysqli_query($connection,"select id,name,price from ecomm where name like '%$q%' or name like '%$q%' order by id LIMIT 5");
     while($row=mysqli_fetch_array($strSQL_Result))
     {
         $name   = $row['name'];
         $price      = $row['price'];
-        $b_name = '<strong>'.$q.'</strong>';
-        $b_price    = '<strong>'.$q.'</strong>';
-        $final_name = str_ireplace($q, $b_name, $name);
-        $final_price = str_ireplace($q, $b_price, $price);
+        
+       
      ?>
             <div class="show" align="left">
-                <span class="name"><?php echo $final_name; ?></span>
+                <span class="name"><?php echo $name; ?></span>
             </div>
         <?php
     }
