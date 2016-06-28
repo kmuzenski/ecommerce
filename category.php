90c0e2d25c3284658db15c32ff066ebf1fe02781
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-27 23:46:53 -0500

    style

--- category.php
+++ category.php
@@ -12,10 +12,10 @@
 <div class="container">
 <div class="row">
 
-<div class="col-xs-2">
+<div class="col-xs-3">
 </div>
 
-<div class="col-xs-6">
+<div class="col-xs-4">
     
               <?php 
               $category_id = $_GET['id'];
@@ -56,7 +56,7 @@
 
 </div>
 
-<div class="col-xs-2">
+<div class="col-xs-3">
 </div>
 
 </div>
